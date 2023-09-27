03b3c4e7749138935711be99587b0a2b0ba69fdd
arman-rjc <AChowdhury@rjc.ca>
2023-09-26 21:55:47 -0700

    Added layout for streamlit app

--- stiffener.py
+++ stiffener.py
@@ -1,1 +1,29 @@
-# I will work on this cripot to check the stiffener capacity
+import forallpeople
+forallpeople.environment('structural', top_level=True)
+import math
+from math import sqrt
+import streamlit as st
+from handcalcs.decorator import handcalc
+
+st.set_page_config(layout='wide')
+
+# Custom CSS for changing the font size, making it bold, and using Times New Roman font
+custom_css = """
+<style>
+    /* Apply to all text in the app */
+    body {
+        font-family: "Times New Roman", Times, serif;
+        font-size: 16px; /* Adjust the font size as needed */
+        font-weight: bold;
+    }
+</style>
+"""
+
+# Display the custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
+
+
+
+
+st.title('**:blue[Stiffener Plate Resistance]**')
+# st.divider()  # 👈 Draws a horizontal rule
