348a8d774d73dba9684a0e79b2f8da88f075620f
arman-rjc <AChowdhury@rjc.ca>
2023-09-26 22:00:06 -0700

    Added disclaimer

--- stiffener.py
+++ stiffener.py
@@ -27,3 +27,28 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 st.title('**:blue[Stiffener Plate Resistance]**')
 # st.divider()  # 👈 Draws a horizontal rule
+
+
+# Define the disclaimer text
+disclaimer_text = """
+**Disclaimer of Warranty and Liability**
+
+By using this app, you acknowledge and agree that the results it provides are for informational purposes only. The engineer using this tool is solely responsible for verifying and validating the accuracy of the results obtained. This app is provided "as is," without any warranty or guarantee of any kind, express or implied. In no event shall the developers or contributors be liable for any damages or consequences arising from the use of this app. You are encouraged to exercise due diligence and professional judgment when relying on the output of this tool.
+"""
+
+# Display the disclaimer text if the checkbox is checked
+st.markdown(disclaimer_text)
+
+# Add a checkbox for users to acknowledge the disclaimer
+disclaimer_accepted = st.checkbox("I have read and agree to the Disclaimer of Warranty and Liability")
+
+
+#
+#  Create a Streamlit app
+st.sidebar.write("Project Information")
+
+# Add text input boxes for Project Name, Job No, Designer, and Date
+project_name = st.sidebar.text_input("Project Name:")
+job_no = st.sidebar.text_input("Job No:")
+designer = st.sidebar.text_input("Designer:")
+date = st.sidebar.text_input("Date:")
