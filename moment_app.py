608f34ceb32a75207682e6287c8ca02913e2708d
arman-rjc <112727494+arman-rjc@users.noreply.github.com>
2025-05-19 07:43:59 -0700

    Update moment_app.py

--- moment_app.py
+++ moment_app.py
@@ -4,6 +4,7 @@ import math
 from math import sqrt
 import streamlit as st
 from handcalcs.decorator import handcalc
+from streamlit.components.v1 import html
 
 #________________________________________________________________________________________________________
 # How to run this app:
@@ -201,3 +202,20 @@ with st.expander("Bar diameter information"):
     image_filename_2 = 'bar_diameter_table.png'  # Replace with the actual image file
     st.image(image_filename_2, caption='Fig 2: Bar diameter and area', width=600)
 
+# Add Buy Me a Coffee button
+floating_button = """
+<div style="position: fixed; bottom: 20px; right: 20px; z-index: 100;">
+<script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" 
+data-name="bmc-button" 
+data-slug="armans01" 
+data-color="#FFDD00" 
+data-emoji=""  
+data-font="Cookie" 
+data-text="Buy me a coffee" 
+data-outline-color="#000000" 
+data-font-color="#000000" 
+data-coffee-color="#ffffff" >
+</script>
+</div>
+"""
+html(floating_button, height=80)
